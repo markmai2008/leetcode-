bff771e2044f021dda496e41f60107df797be9cb
markmai2008 <3263549762@qq.com>
2020-10-01 10:14:55 +0800

    0189

---
+++ leetcode-0189.py
@@ -0,0 +1,56 @@
+from typing import List
+
+
+class Solution:
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            nums.insert(0, nums.pop())
+
+
+if __name__ == "__main__":
+    """测试代码
+    """
+
+    cases = [
+        (([1, 2, 3, 4, 5, 6, 7], 3), [5, 6, 7, 1, 2, 3, 4]),
+        (([-1, -100, 3, 99], 2), [3, 99, -1, -100])
+
+    ]
+
+    solution = Solution()
+
+    all_result = ''
+
+    for case, (i, o) in enumerate(cases):
+        print(f'#样例: {case+1}')
+        print(f'输入: {repr(i)}')
+        print(f'期望输出: {repr(o)}')
+        try:
+            solution.rotate(*i)
+            result = i[0]
+            correct = result == o
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            correct = False
+        all_result += '.' if correct else 'x'
+        print(f'我的输出: {repr(result)}')
+        print('结果: ' + ('正确' if correct else '错误'))
+        print()
+
+    print('测试结果:', all_result)
+    yes_count = sum([1 if x == '.' else 0 for x in all_result])
+    no_count = sum([1 if x == 'x' else 0 for x in all_result])
+    print(
+        f'共测试 {len(all_result)} 个样例，'
+        f'成功 {yes_count} 个，'
+        f'失败 {no_count} 个'
+    )
+    if 'x' in all_result:
+        print('测试失败，请检查你的代码！')
+    else:
+        print('测试通过，所有的测试数据均返回了正确的答案！')
